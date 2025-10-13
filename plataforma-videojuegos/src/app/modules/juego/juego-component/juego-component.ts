285754475574864b259ecca08668614161af2b96
EricNahuel2002 <ericaquino2002@gmail.com>
2025-10-13 16:48:33 -0300

    se agregan las reseñas de los juegos

--- plataforma-videojuegos/src/app/modules/juego/juego-component/juego-component.ts
+++ plataforma-videojuegos/src/app/modules/juego/juego-component/juego-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DetalleJuegoComponent } from '../detalle-juego-component/detalle-juego-component';
 import { OpcionesComponent } from '../opciones-component/opciones-component';
+import { ValoracionesComponent } from '../valoraciones-component/valoraciones-component';
 
 @Component({
   selector: 'app-juego-component',
-  imports: [DetalleJuegoComponent, OpcionesComponent],
+  imports: [DetalleJuegoComponent, OpcionesComponent,ValoracionesComponent],
   templateUrl: './juego-component.html',
   styleUrl: './juego-component.css'
 })
---
+++ plataforma-videojuegos/src/app/modules/juego/valoraciones-component/valoraciones-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-valoraciones-component',
+  imports: [CardModule, ButtonModule],
+  templateUrl: './valoraciones-component.html',
+  styleUrl: './valoraciones-component.css'
+})
+export class ValoracionesComponent {
+
+}
