ffeccc1d172af37600993000ae0ddc18aa72a799
Kvnrot-sys <kvnrotela@gmail.com>
2025-10-13 20:12:43 -0300

    usuarioPerfil maquetado

--- plataforma-videojuegos/src/app/app.routes.ts
+++ plataforma-videojuegos/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './modules/home-component/home-component';
 import { JuegoComponent } from './modules/juego/juego-component/juego-component';
 import { RegistroComponent } from './modules/registro/registro.component';
 import { IniciarSesionComponent } from './modules/iniciar-sesion/iniciar-sesion.component';
+import { UsuarioPerfilComponent } from './modules/usuario-perfil/usuario-perfil.component';
+
 
 
 export const routes: Routes = [
@@ -10,6 +12,7 @@ export const routes: Routes = [
     { path: 'registro', component: RegistroComponent },
     { path: 'juego', component : JuegoComponent },
     {path: 'iniciar-sesion', component: IniciarSesionComponent},
+    {path: 'usuario-perfil',component: UsuarioPerfilComponent},
     { path: '**', redirectTo: '' } 
 ];
 
