df1f8fbfebf3be281f0a3c5521d6287b45184117
Martin <ursomartin89@gmail.com>
2025-11-13 11:59:57 -0300

    Control de sesiones y pagina de inicio

---
+++ plataforma-videojuegos/src/app/servicios/noAuthGuard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { UsuarioService } from './usuario.service';
+
+@Injectable({ providedIn: 'root' })
+export class NoAuthGuard implements CanActivate {
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    const id = this.usuarioService.obtenerUsuarioDeSesion(); // síncrono
+    if (!id) return true;
+    return this.router.createUrlTree(['/catalogo']);
+  }
+}
